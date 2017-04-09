8787db1b70a7861cf72654e2edc5895b9641b8c1
Brian Leschke <Bleschke@users.noreply.github.com>
2017-04-09 18:48:17 -0400

    Update main.ino

--- src/main.ino
+++ src/main.ino
@@ -1,1 +1,19 @@
-
+/* 
+ * Brian Leschke
+ * April 9, 2017
+ * Adafruit Huzzah WMATA ESP8266 Metro Status
+ * An ESP8266 will control a neopixel ring (metro line), 7-segment LED (arrival time), and 16x4 LCD screen (station updates).
+ * Version *.*
+ * 
+ *
+ * -- Credit and Recognition: --
+ * 
+ *
+ * -- Changelog: -- 
+ * 
+ * 4/9/17 - Create .ino and data files
+ * // - 
+ * // - 
+ *
+ * 
+*/
